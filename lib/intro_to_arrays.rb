35d554d082ab1d2f60c946a18d68d6e4e060ee1a
John Cho <johnycho16@gmail.com>
2019-03-01 21:30:36 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -15,10 +15,17 @@ def third_element(array)
 end
 
 def last_element(array)
-  array.last
+  array[-1]
 end
 
 def first_element_with_array_methods(array)
   array.first
 end
 
+def last_element_with_array_methods(array)
+  array.last
+end
+
+def length_of_array(array)
+  array.length
+end

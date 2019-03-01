ed2c8490ebc3b04987648e4ecaef3ed3b070decf
John Cho <johnycho16@gmail.com>
2019-03-01 21:28:34 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,24 @@
+def instantiate_new_array
+  array = []
+end
+
+def array_with_two_elements
+  array = [1,2]
+end
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array.last
+end
+
+def first_element_with_array_methods(array)
+  array.first
+end
+
